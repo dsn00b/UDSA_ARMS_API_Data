ad2a15c0a26500426e2bafd0d8b2f9a5695bd975
Rojan Karakaya <rojankarakaya@gmail.com>
2020-10-01 15:59:20 +0200

    some tests for input to pull_data

--- tests/testthat/test-pulldata.r
+++ tests/testthat/test-pulldata.r
@@ -1,9 +1,49 @@
 context("pull data")
 
-test_that("erroneous report name", {
+test_that("erroneous report name throws error", {
+  
+  expect_error( pull_data(c(2000,"2001"),report="blabla") )
+  
+})
+
+
+test_that("correct report name and year input does not throw error", {
+  
+  expect_error( pull_data(c(2000,"2001"),report="Farm Business Balance Sheet"), NA )
+  
+})
+
+test_that("incorrect input for year throws error", {
+  
+  expect_error( pull_data(c(2000,"2001k"),report="Farm Business Balance Sheet") )
+  
+})
+
+test_that("incorrect input for year throws error", {
   
   expect_error( pull_data(c(2000,"2001"),report="Farm Business Balance Sheet") )
   
 })
+
+
+test_that("incorrect input for state throws error", {
+  
+  expect_error( pull_data(c(2000,"2001"),report="Farm Business Balance Sheet", state="SE") )
+  
+})
+
+
+test_that("state='all' does not throw error", {
+  
+  expect_error( pull_data(c(2000,"2001"),report="Farm Business Balance Sheet", state="all"), NA )
+  
+})
+
+test_that("state='all' does not throw error", {
+  
+  expect_error( pull_data(c(2000,"2001"),report="Farm Business Balance Sheet", state="all"), NA )
+  
+})
+
 
 
